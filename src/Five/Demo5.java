fe59728cd9ef1a627e3de690ae9993a7a0adc8e0
15718 <15718@LAPTOP-SS0766RE>
2021-11-22 21:59:25 -0500

    gethub fun...

---
+++ src/Five/Demo5.java
@@ -0,0 +1,10 @@
+package Five;
+
+public class Demo5 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Demo Five");
+	}
+
+}
---
+++ src/Work5/Day5.java
@@ -0,0 +1,5 @@
+package Work5;
+
+public class Day5 {
+
+}
